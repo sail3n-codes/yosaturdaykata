a62a868038b12ca843230636aabccf05f05d1062
Sailendra <sailencodes@gmail.com>
2025-06-19 23:24:20 +0545

    font updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-   <>Hello World</>
+    <>
+      <h1 className="">Hello World</h1>
+      <h2 className="">Hello World</h2> <h3 className="">Hello World</h3>{" "}
+      <h4 className="">Hello World</h4> <h5 className="">Hello World</h5>{" "}
+      <h6 className="">Hello World</h6>
+      <p className="">Hello World</p>
+    </>
   );
 }
