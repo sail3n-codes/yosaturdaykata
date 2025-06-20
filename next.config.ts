f7c2304e7c3655a1583ca5d331273de7679e9ff8
Sailendra <sailencodes@gmail.com>
2025-06-20 23:49:56 +0545

    update next config to fix image loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: "/yosaturdaykata",
+  assetPrefix: "/yosaturdaykata",
 };
 
 export default nextConfig;
