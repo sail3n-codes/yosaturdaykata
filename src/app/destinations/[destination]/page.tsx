13fcdd16a54ee06d75d834b6321588226e3ae8d8
Sailendra <sailencodes@gmail.com>
2025-06-22 15:34:15 +0545

    added generate static params to fix build issue

--- src/app/destinations/[destination]/page.tsx
+++ src/app/destinations/[destination]/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function DestinationDetail() {
-  return <div>page</div>;
+interface Props {
+  params: { destination: string };
+}
+
+export default function DestinationDetail({ params }: Props) {
+  return <div>page :{params.destination}</div>;
+}
+
+export async function generateStaticParams() {
+  return [
+    { destination: "paris" },
+    { destination: "tokyo" },
+    { destination: "kathmandu" },
+    // Add all your expected static paths here
+  ];
 }
